f2fe607cde7cdc3b0cb405c6ba8f176ead7793ff
ashad nasim <ashadnasim123@gmail.com>
2023-12-20 09:25:38 +0400

    changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,5 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+// export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = "";
 
 // auth
 export const SIGNIN = "/api/v1/auth/signin";
